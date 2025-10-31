1683fbd343d90bb0c3cff0b91067874816b77aa4
Strong214356 <tidiandelage1@gmail.com>
2025-10-31 13:45:34 +0100

    try fix

--- js/pages/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Index = void 0;
-const page_js_1 = require("../page.js");
-exports.Index = new page_js_1.Page(() => {
-    return `
-        <p>Hello You!</p>
-        `;
-});
