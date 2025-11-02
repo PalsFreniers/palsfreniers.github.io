1e812bfa7ede6312293dbda1259af6d01423e273
Strong214356 <tidiandelage1@gmail.com>
2025-11-02 07:41:22 +0100

    try and fix listeners

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import { Router } from './router.js';
 import { Index } from './pages/index.js';
 import { E404 } from './pages/errors.js';
@@ -14,15 +5,13 @@ const nav = new Router();
 nav.add('/', Index);
 nav.add('/home', Index);
 nav.add('/404', E404);
-window.addEventListener('DOMContentLoaded', () => __awaiter(void 0, void 0, void 0, function* () {
-    document.getElementsByName('a').forEach((val) => {
-        val.addEventListener('click', (e) => {
-            e.preventDefault();
-            const url = val.getAttribute('href');
-            if (url) {
-                nav.warp(url);
-            }
-        });
+document.getElementsByName('a').forEach((val) => {
+    val.addEventListener('click', (e) => {
+        e.preventDefault();
+        const url = val.getAttribute('href');
+        if (url) {
+            nav.warp(url);
+        }
     });
-    nav.warp();
-}));
+});
+nav.warp();
