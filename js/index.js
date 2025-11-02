84c8ee3da933c92f941dcd729f4fbea85c7c2ce5
Strong214356 <tidiandelage1@gmail.com>
2025-11-02 07:46:40 +0100

    I don't understand why id won't work

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ nav.add('/', Index);
 nav.add('/home', Index);
 nav.add('/404', E404);
 document.getElementsByName('a').forEach((val) => {
+    console.log(val);
     val.addEventListener('click', (e) => {
         e.preventDefault();
         const url = val.getAttribute('href');
