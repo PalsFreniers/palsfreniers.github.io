c6e7c6d30ba0b7f01de57adaf521b194dad34678
Strong214356 <tidiandelage1@gmail.com>
2025-11-03 19:31:31 +0100

    fixing projects link

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import Router from './router.js';
 import Index from './pages/index.js';
 import Projects from './pages/projects.js';
 import Contact from './pages/contact.js';
+import Project from './pages/project.js';
 import * as Errors from './pages/errors.js';
 export const nav = new Router();
 nav.add('/', Index);
@@ -9,7 +10,7 @@ nav.add('/home', Index);
 nav.add('/404', Errors.E404);
 nav.add('/contact', Contact);
 nav.add('/projects', Projects);
-nav.add('/project', Projects);
+nav.add('/project', Project);
 if (!localStorage.getItem("lang")) {
     localStorage.setItem("lang", "fr");
 }
