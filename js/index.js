5a48abbefc66a8e0f2a6ba6ca68a75e6f65dd4e2
Strong214356 <tidiandelage1@gmail.com>
2025-11-03 14:36:49 +0100

    added projects to the router

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 import Router from './router.js';
 import Index from './pages/index.js';
-import * as Errors from './pages/errors.js';
+import Projects from './pages/projects.js';
 import Contact from './pages/contact.js';
+import * as Errors from './pages/errors.js';
 const nav = new Router();
 nav.add('/', Index);
 nav.add('/home', Index);
 nav.add('/404', Errors.E404);
 nav.add('/contact', Contact);
+nav.add('/projects', Projects);
 if (!localStorage.getItem("lang")) {
     localStorage.setItem("lang", "fr");
 }
