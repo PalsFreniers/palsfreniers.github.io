4c9f42378d4c0614c8bdb72b2828f88a54e52acc
Strong214356 <tidiandelage1@gmail.com>
2025-11-04 13:03:58 +0100

    trying to make a caroussel but I am bad a coding

--- js/pages/project.js
+++ js/pages/project.js
@@ -30,17 +30,16 @@ function makeProjectTitle(lang, prog) {
 function makeCarousel(project, prog) {
     return __awaiter(this, void 0, void 0, function* () {
         let ret = `
-        <div class="project-entry-carousel">
+        <div class="project-entry-carousel" id="carousel">
                 <h3 class="project-entry-carousel">Images</h3>
-                <div class="project-entry-carousel-imgs">`;
-        for (let i = 0; i < project.images.length; i++) {
-            ret += `
-                <div class="slide">
-                        <img alt="${project.images[i]}" src="${urls.githubProjectsFilesURL}/${prog}/images/${i + 1}.png" />
+                <div class="project-entry-carousel-imgs">
+                        <button id="prev-slide" class="prev-slide slide-btn"><</button>
+                        <div class="slide" data-slidenum="0">
+                                <img id="carousel-img" alt="${project.images[0]}" src="${urls.githubProjectsFilesURL}/${prog}/images/1.png" />
+                        </div>
+                        <button id="next-slide" class="next-slide slide-btn"><</button>
                 </div>
-                `;
-        }
-        ret += '</div></div>';
+        </div>`;
         return ret;
     });
 }
@@ -70,5 +69,53 @@ const Project = new Page((arg) => __awaiter(void 0, void 0, void 0, function* ()
                 ${yield makeDescription(lang, arg)}
         </div>
         `;
+}), (arg) => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield fetch(`${urls.projectsFilesURL}/${arg}/info.json`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const project = yield res.json();
+    const prevBtn = document.getElementById('prev-slide');
+    if (!prevBtn) {
+        console.error('unable to find prev Button');
+        return;
+    }
+    const nextBtn = document.getElementById('next-slide');
+    if (!nextBtn) {
+        console.error('unable to find next Button');
+        return;
+    }
+    const slide = document.getElementById('slide');
+    if (!slide) {
+        console.error('unable to find next Button');
+        return;
+    }
+    const img = document.getElementById('carousel-img');
+    if (!img) {
+        console.error('unable to find next Button');
+        return;
+    }
+    prevBtn.addEventListener('click', () => {
+        const count = project.images.length;
+        let currentSlide = parseInt(slide.getAttribute("data-slidenum") || '0');
+        if (currentSlide == 0)
+            currentSlide = count;
+        currentSlide--;
+        img.setAttribute('src', `${urls.githubProjectsFilesURL}/${arg}/images/${currentSlide + 1}.png`);
+        img.setAttribute('alt', `${project.images[currentSlide]}`);
+        slide.setAttribute('data-slidenum', `${currentSlide}`);
+    });
+    nextBtn.addEventListener('click', () => {
+        const count = project.images.length;
+        let currentSlide = parseInt(slide.getAttribute("data-slidenum") || '0');
+        if (currentSlide == count - 1)
+            currentSlide = 0;
+        currentSlide++;
+        img.setAttribute('src', `${urls.githubProjectsFilesURL}/${arg}/images/${currentSlide + 1}.png`);
+        img.setAttribute('alt', `${project.images[currentSlide]}`);
+        slide.setAttribute('data-slidenum', `${currentSlide}`);
+    });
 }));
 export default Project;
